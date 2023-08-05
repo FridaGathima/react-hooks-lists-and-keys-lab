425e66057b92d66218ca2553776eb980925631f5
FridaGathima <frida.gathima@student.moringaschool.com>
2023-08-05 22:19:25 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  const linkCards = links.map((linkObj, index) => (
-    <a key = {index} href={linkObj}>{linkObj}</a>
-  ))
-
   // return <nav>{/* display an <a> tag for each link here */}</nav>;
   return <nav>
-    {linkCards}
+    {links.map((linkObj, index) => (
+    <a key = {index} href={`#${linkObj}`}>{linkObj}</a>
+  ))}
   </nav>
 }
 
